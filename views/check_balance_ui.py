bc99370f665a6ad463df8186a1b1e1ea0e9aef1c
thanhpham312 <tpham74@my.bcit.ca>
2018-04-02 18:14:38 -0700

    Added balance check interface and a way to draw them in main UI

--- views/check_balance_ui.py
+++ views/check_balance_ui.py
@@ -1,10 +1,41 @@
 from tkinter import *
+from constants import *
 
 class CheckBalance():
-    def __init__(self, master, account):
+    def __init__(self, master, account=None):
         self.master = master
 
         # Frames:
+        self.info_screen = Frame(self.master, bg='blue')
+        self.buttons_frame = Frame(self.master, bg='red')
 
+        # Elements:
+        self.label1 = Label(self.info_screen, text='Your account balance is:')
+        self.label2 = Label(self.info_screen, text='$0')
+        self.button1 = Button(self.buttons_frame, text='CANCEL')
+        self.button2 = Button(self.buttons_frame, text='OK')
 
         # Gridding:
+        self.master.rowconfigure(0, weight=1)
+        self.master.columnconfigure(0, weight=1)
+        self.info_screen.rowconfigure(0, weight=1)
+        self.info_screen.rowconfigure(1, weight=1)
+        self.info_screen.columnconfigure(0, weight=1)
+
+        self.buttons_frame.rowconfigure(0, weight=1)
+        self.buttons_frame.rowconfigure(1, weight=1)
+        self.buttons_frame.columnconfigure(0, weight=1)
+        self.buttons_frame.columnconfigure(1, weight=1)
+
+        self.info_screen.grid(row=0, column=0, padx=20, pady=20, sticky=N+S+E+W)
+        self.buttons_frame.grid(row=1, column=0, padx=20, pady=20, sticky=N+S+E+W)
+
+        self.label1.grid(row=0, column=0, padx=20, pady=20, sticky=N+S+E+W)
+        self.label2.grid(row=1, column=0, padx=20, pady=20, sticky=N+S+E+W)
+        self.button1.grid(row=0, column=0, padx=20, pady=20, sticky=N+S+E+W)
+        self.button2.grid(row=0, column=1, padx=20, pady=20, sticky=N+S+E+W)
+
+if __name__ == '__main__':
+    root = Tk()
+    CheckBalance(root)
+    mainloop()
--- views/main_ui.py
+++ views/main_ui.py
@@ -1,10 +1,14 @@
 from tkinter import *
-from .check_balance_ui import CheckBalance
+from constants import *
+from check_balance_ui import CheckBalance
 
 class MainUI():
     def __init__(self, master):
         self.master = master
         self.master.title('Bank ATM')
+        self.master.geometry(str(WINDOW_WIDTH) + 'x' + str(WINDOW_HEIGHT))
+
+        self.current_frame = NONE
 
         # Menu:
         self.main_menu = Menu(self.master)
@@ -15,16 +19,23 @@ class MainUI():
         self.file_menu.add_command(label='Quit', command=self.master.quit)
 
         # Frames:
-        self.upper_interface_frame = Frame(self.master)
-        self.bottom_buttons_frame = Frame(self.master)
+        self.upper_interface_frame = Frame(self.master, bg='green')
+        self.bottom_buttons_frame = Frame(self.master, bg='black')
 
-        self.check_balance = CheckBalance(self.middle_interface_frame)
+        # self.check_balance = CheckBalance(self.upper_interface_frame)
         # Gridding:
+        self.master.rowconfigure(0, weight=1)
+        self.master.columnconfigure(0, weight=1)
 
-        self.upper_interface_frame.grid(row=0, column=0, padx=20, pady=10)
-        self.bottom_buttons_frame.grid(row=1, column=0, padx=20, pady=10)
+        self.upper_interface_frame.grid(row=0, column=0, padx=20, pady=10,  sticky=N+S+E+W)
+        self.bottom_buttons_frame.grid(row=1, column=0, padx=20, pady=10,  sticky=N+S+E+W)
+
+    def draw_interface(self):
+        self.current_frame = CheckBalance(self.upper_interface_frame)
 
 if __name__ == '__main__':
     root = Tk()
-    MainUI(root)
+    # root.geometry('600x450')
+    new_window = MainUI(root)
+    new_window.draw_interface()
     mainloop()

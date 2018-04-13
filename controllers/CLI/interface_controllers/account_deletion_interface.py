1de5fad36bf182205c59d0c7c45d3162573d75c2
ViktorSheverdin <viktor.sheverdin@gmail.com>
2018-04-13 16:55:42 -0700

    Delettion bug fixed

--- controllers/CLI/interface_controllers/account_deletion_interface.py
+++ controllers/CLI/interface_controllers/account_deletion_interface.py
@@ -8,7 +8,8 @@ class AccountDeletionController():
         self.delete_account()
 
     def delete_account(self):
-        while 1:
-            customer_id = input("Enter the customer id: ")
-            self.main_controller.customer_model.delete_customer(customer_id)
-            self.main_controller.change_controller('main_menu')
+        customer_id = input('Please enter a customer id: ')
+        account_type = input('Enter the account type: 1-chequing; 2-saving: ')
+        self.main_controller.customer_model.delete_customer_account(customer_id, account_type)
+        print('Customer deletion successful!')
+        self.main_controller.change_controller('main_menu')
--- controllers/CLI/interface_controllers/customer_deletion_controller.py
+++ controllers/CLI/interface_controllers/customer_deletion_controller.py
@@ -8,8 +8,7 @@ class CustomerDeletionController():
         self.delete_customer()
 
     def delete_customer(self):
-        customer_id = input('Please enter a name for new customer: ')
-        account_type = input('Choose a pin: ')
-        self.main_controller.customer_model.delete_customer_account(customer_id, account_type)
-        print('Customer deletion successful!')
-        self.main_controller.change_controller('main_menu')
+        while 1:
+            customer_id = input("Enter the customer id: ")
+            self.main_controller.customer_model.delete_customer(customer_id)
+            self.main_controller.change_controller('main_menu')
